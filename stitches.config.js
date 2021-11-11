417416e4672594c768de41936ca04416a290489b
Sean Ho <seanho96@yahoo.com>
2021-11-11 21:29:36 +0800

    chore: install svg loader, stitches, radix-ui and configure for nextjs

---
+++ stitches.config.js
@@ -0,0 +1,9 @@
+import { createStitches } from "@stitches/react";
+
+export const { styled, getCssText, CSS, keyframes } = createStitches({
+  theme: {
+    fonts: {},
+    colors: {},
+    fontSizes: {},
+  },
+});
