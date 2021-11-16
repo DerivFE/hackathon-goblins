3c1824653957cb0044750bed0ae4c14f9f532c97
matin-binary <matin@binary.com>
2021-11-16 12:31:50 +0800

    Chore: Added Bug bounty page

---
+++ pages/docs/bug-bounty/index.js
@@ -0,0 +1,42 @@
+import Layout from "components/layout/Layout";
+import css from "./BugBounty.module.css";
+
+const BugBounty = () => (
+  <Layout>
+    <div className={css.page_content}>
+      <h1 className={css.page_title}>Bug bounty</h1>
+      <div className={css.text_block}>
+        <h3 className={css.subtitle}>Want to help us enhance our security?</h3>
+        <p>
+          Test our products and services for security vulnerabilities and earn a
+          monetary reward for any verifiable issues that you find, courtesy of
+          our bug bounty programme.
+        </p>
+      </div>
+      <div className={css.cta}>
+        <div className={css.cta_grid}>
+          <div className={css.cta_grid_box}>
+            <h2>Explore our bounty programme</h2>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://hackerone.com/binary"
+            >
+              <div className={css.cta_button}>Go to Hackerone</div>
+            </a>
+          </div>
+          <div className={css.cta_grid_box}>
+            <h2>Got questions?</h2>
+            <p>
+              Email us at
+              <a href="mailto:security@deriv.com"> security@deriv.com</a> for
+              more information.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Layout>
+);
+
+export default BugBounty;
