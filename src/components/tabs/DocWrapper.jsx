41ccd8e4b47460f318b6dfadf9904ef2929c4398
Akmal Djumakhodjaev <akmal@binary.com>
2021-11-23 16:59:00 +0800

    Add mobile dropdown component

--- src/components/tabs/DocWrapper.jsx
+++ src/components/tabs/DocWrapper.jsx
@@ -1,47 +1,85 @@
 import React from "react";
 import {
-    CustomTabs as Tabs,
-    TabsHeader,
-    TabsList,
-    TabsTrigger,
-    TabsContent,
-  } from "components/tabs/CustomTabs";
+  CustomTabs as Tabs,
+  TabsHeader,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "components/tabs/CustomTabs";
+import DocumentationSelector from "components/DocumentationSelector";
 import { styled } from "stitches.config";
 
-const StyledListWrapper = styled('div', {
-  position: 'fixed',
+const StyledListWrapper = styled("div", {
+  position: "fixed",
   width: 220,
 });
 
-const StyledDiv = styled('div', {
-  minHeight: '95vh',
-  width: '80%',
-  alignSelf: 'center',
-  padding: '105px 0 80px',
+const StyledDiv = styled("div", {
+  minHeight: "95vh",
+  width: "80%",
+  alignSelf: "center",
+  padding: "105px 0 80px",
 });
 
 const DocWrapper = ({ children, value, ...props }) => {
   return (
-    <StyledDiv css={{
-        '@tabletL' : {
-          padding: '19px 0 0'
+    <StyledDiv
+      css={{
+        "@tabletL": {
+          padding: "19px 0 0",
         },
-     }}>
-        <Tabs value={value} orientation="vertical" {...props}>
-          <TabsList aria-label={value} css={{ '@tabletL': { display: 'none'}}}>
-            <StyledListWrapper>
-              <TabsHeader>Deriv API</TabsHeader>
-              <TabsTrigger value="/docs" active={getActiveState('/docs', value)}>Quickstart</TabsTrigger>
-              <TabsTrigger value="/docs/app-registration" active={getActiveState('/docs/app-registration', value)}>App registration</TabsTrigger>
-              <TabsTrigger value="/playground" active={getActiveState('/playground', value)}>API playground</TabsTrigger>
-              <TabsTrigger value="/docs/api-guide" active={getActiveState('/docs/api-guide', value)}>API guide</TabsTrigger>
-              <TabsTrigger value='/docs/faq' active={getActiveState('/docs/faq', value)}>FAQ</TabsTrigger>
-              <TabsTrigger value="/docs/json-schemas" active={getActiveState('/docs/json-schemas', value)}>JSON Schemas</TabsTrigger>
-              <TabsTrigger value="/docs/bug-bounty" active={getActiveState('/docs/bug-bounty', value)}>Bug Bounty</TabsTrigger>
-            </StyledListWrapper>
-          </TabsList>
-          <TabsContent value={value}>{children}</TabsContent>
-        </Tabs>
+      }}
+    >
+      <Tabs value={value} orientation="vertical" {...props}>
+        <TabsList aria-label={value} css={{ "@tabletL": { display: "none" } }}>
+          <StyledListWrapper>
+            <TabsHeader>Deriv API</TabsHeader>
+            <TabsTrigger value="/docs" active={getActiveState("/docs", value)}>
+              Quickstart
+            </TabsTrigger>
+            <TabsTrigger
+              value="/docs/app-registration"
+              active={getActiveState("/docs/app-registration", value)}
+            >
+              App registration
+            </TabsTrigger>
+            <TabsTrigger
+              value="/playground"
+              active={getActiveState("/playground", value)}
+            >
+              API playground
+            </TabsTrigger>
+            <TabsTrigger
+              value="/docs/api-guide"
+              active={getActiveState("/docs/api-guide", value)}
+            >
+              API guide
+            </TabsTrigger>
+            <TabsTrigger
+              value="/docs/faq"
+              active={getActiveState("/docs/faq", value)}
+            >
+              FAQ
+            </TabsTrigger>
+            <TabsTrigger
+              value="/docs/json-schemas"
+              active={getActiveState("/docs/json-schemas", value)}
+            >
+              JSON Schemas
+            </TabsTrigger>
+            <TabsTrigger
+              value="/docs/bug-bounty"
+              active={getActiveState("/docs/bug-bounty", value)}
+            >
+              Bug Bounty
+            </TabsTrigger>
+          </StyledListWrapper>
+        </TabsList>
+        <TabsContent value={value}>
+          <DocumentationSelector css={{ "@tabletL": { display: "block" } }} />
+          {children}
+        </TabsContent>
+      </Tabs>
     </StyledDiv>
   );
 };
